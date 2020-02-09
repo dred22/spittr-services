a9603c60a22328ef9ea18a9e842b6ff42530337f
Magomed KADIEV <jhabitey@gmail.com>
2020-02-09 20:14:18 +0100

    spittr domain initialized

---
+++ spittr-domain/src/main/java/spittr/domain/converter/LocalDateAttributeConverter.java
@@ -0,0 +1,19 @@
+package spittr.domain.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Converter
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+    @Override
+    public Date convertToDatabaseColumn(LocalDate locDate) {
+        return (locDate == null ? null : Date.valueOf(locDate));
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date sqlDate) {
+        return (sqlDate == null ? null : sqlDate.toLocalDate());
+    }
+}
