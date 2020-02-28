8c4a97cbade0dd44f6af66b9ca61f7da1f2611fb
Magomed KADIEV <magomed.kadiev@olea-medical.com>
2020-02-28 20:15:59 +0100

    web config added

--- spittr-web/src/main/java/spittr/web/WebApp.java
+++ spittr-web/src/main/java/spittr/web/WebApp.java
@@ -2,12 +2,11 @@ package spittr.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 /*@EnableJpaRepositories("spittr.data")
 @EntityScan("spittr.domain.model")*/
-public class WebApp extends SpringBootServletInitializer {
+public class WebApp {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApp.class, args);
