898866c1c1080987ca4c8a735a2b5fb15b6f2b75
Magomed KADIEV <magomed.kadiev@olea-medical.com>
2020-03-06 13:56:36 +0100

    applying thymeleaf tags onto web sources. Index page already functional

--- spittr-web/src/main/java/spittr/web/config/WebConfig.java
+++ spittr-web/src/main/java/spittr/web/config/WebConfig.java
@@ -1,9 +1,13 @@
 package spittr.web.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +15,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(10);
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.displayName());
+        //messageSource.setUseCodeAsDefaultMessage(true);;
+        return messageSource;
     }
 }
