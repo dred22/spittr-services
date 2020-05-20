f6dd532ab52bce54e56f3b079ffe6016df0858ce
Magomed KADIEV <jhabitey@gmail.com>
2020-05-20 09:14:47 +0200

    config server implementing

---
+++ spittr-config-server/src/main/java/spittr/spittrconfigserver/SpittrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package spittr.spittrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpittrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpittrConfigServerApplication.class, args);
+	}
+
+}
---
+++ spittr-config-server/src/test/java/spittr/spittrconfigserver/SpittrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package spittr.spittrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpittrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
